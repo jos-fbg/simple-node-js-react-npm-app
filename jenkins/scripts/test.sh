8d70b6b794c3860f1b33d6489a7e66db589990d5
Jonas <jonassvendsen1998@hotmail.com>
2021-03-22 08:38:47 +0100

    test stage npm install

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -9,8 +9,10 @@ echo 'purposes of this tutorial, this flag is not important. However, when'
 echo 'installing this dependency, it would typically be done so using this'
 echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
+echo 'Test meme'
+
 set -x
-# npm install --save-dev cross-env
+npm install --save-dev cross-env
 set +x
 
 echo 'The following "npm" command tests that your simple Node.js/React'
