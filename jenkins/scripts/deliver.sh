d408646e5b5f2205f9baaf2a0d387801bcecdd35
Jonas <jonassvendsen1998@hotmail.com>
2021-03-24 09:31:24 +0100

    asd

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -6,9 +6,9 @@ echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
 
-#set -x
-#npm run build
-#set +x
+set -x
+npm run build
+set +x
 
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
